f17c3bd15ee623a3578a6ee41236a5cca9c63df9
Łukasz Potoczniak <lukaszpotoczniak@gmail.com>
2020-04-19 09:54:26 +0200

    add exercise 15.5

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import styles from './App.scss';
-import List from '../List/List'
+import List from '../List/List';
+import {pageContents, listData} from '../../data/dataStore';
 
 class App extends React.Component {
   render() {
     return (
       <main className={styles.component}>
-        <h1 className={styles.title}>witaj potezny React</h1>
-        <h2 className={styles.subtitle}>Hello world!</h2>
+        <h1 className={styles.title}>{pageContents.title}witaj potezny React</h1>
+        <h2 className={styles.subtitle}>{pageContents.subtitle}Hello world!</h2>
         <List title={['things to do', <sup key='1'>soon!</sup>]} 
-              screen = {'http://uploads.kodilla.com/bootcamp/fer/11.react/space.png'}>
+              screen = {'http://uploads.kodilla.com/bootcamp/fer/11.react/space.png'}
+              {...listData}>
           
         </List>
       </main>
