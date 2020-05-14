a44b41e914f142831f01b6a52137dee3b1d87aeb
Łukasz Potoczniak <lukaszpotoczniak@gmail.com>
2020-05-14 18:35:19 +0200

    add SR but not working

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,37 +1,57 @@
-import React, { Component } from 'react';
+
+import React, {Component} from 'react';
 import styles from './SearchResults.scss';
 import PropTypes from 'prop-types';
 import Card from '../Card/Card.js';
-import Icon from '../Icon/Icon.js';
+import Container from '../Container/Container';
+import connect from 'react-redux';
 import { settings } from '../../data/dataStore';
+import { getCards } from '../../redux/cardsRedux';
+import Icon from '../Icon/Icon';
+
 
 class SearchResults extends Component {
+    static propTypes = {
+      cards: PropTypes.array || [],
+      title: PropTypes.string,
+      addCard: PropTypes.func,
+    }
 
-  static defaultProps = {
-    icon: settings.defaultColumnIcon,
-  }
+    static defaultProps = {
+      icon: settings.defaultColumnIcon,
+      cards: [],
+    }
+    render() {
+      const title = this.props.match.params.title;
 
-  static propTypes = {
-    cards: PropTypes.array,
-    title: PropTypes.string,
-    addCard: PropTypes.func,
-  }
-
-  render() {
-    const {icon} = settings.search;
-    const {cards} = this.props;
-    return (
-      <section className={styles.component}>
-        <h3 className={styles.title}><span className={styles.icon}><Icon icon={icon}/></span></h3>
-        <h1 className={styles.title}>Your search results</h1>
-        <div className={styles.component}>
-          {cards.map(cardData => (
-            <Card key={cardData.id} {...cardData} />
-          ))}
-        </div>
-
-      </section>
-    );
-  }
+      const {cards} = this.props;
+      const {icon} = settings.search;
+      
+      
+      return (
+        <Container>
+          <section className={styles.component}>
+            <h3 className={styles.title}> <span> <Icon icon={icon}/></span></h3>
+            <h1 className={styles.title}>Your search results</h1>
+            <div className={styles.cards}>
+              {cards.map(cardsData => {
+                return <Card key={cardsData.id} title={cardsData.title}/>;
+              }
+              )}
+            </div>
+          </section>
+        </Container>
+        
+      );
+    }
 }
-export default SearchResults;
+
+const mapStateToProps = (state, props) => {
+  const title = props.match.params.title;
+  return {
+    cards: getCards(state,title) ,
+  };
+};
+
+
+export default connect(mapStateToProps) (SearchResults);
